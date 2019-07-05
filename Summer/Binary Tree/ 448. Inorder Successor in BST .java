8fd7c6e41df1967b5ea774f7450d8f58dbe6af12
liuquan <hansxyz08@gmail.com>
2019-07-05 17:44:03 +0800

    Binary Tree Update

---
+++ Summer/Binary Tree/ 448. Inorder Successor in BST .java
@@ -0,0 +1,72 @@
+/*448. Inorder Successor in BST
+中文English
+Given a binary search tree (See Definition) and a node in it, find the in-order successor of that node in the BST.
+
+If the given node has no in-order successor in the tree, return null.
+
+Example
+Example 1:
+
+Input: {1,#,2}, node with value 1
+Output: 2
+Explanation:
+  1
+   \
+    2
+Example 2:
+
+Input: {2,1,3}, node with value 1
+Output: 2
+Explanation: 
+    2
+   / \
+  1   3
+Binary Tree Representation
+
+Challenge
+O(h), where h is the height of the BST.
+
+Notice
+It's guaranteed p is one node in the given tree. (You can directly compare the memory address to find p)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param root: The root of the BST.
+     * @param p: You need find the successor node of p.
+     * @return: Successor of p.
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        TreeNode successor = null;
+        if (root == null){
+            return null;
+        }
+        while (root != null && root.val != p.val){
+            if (root.val > p.val){
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        if (root.right == null){
+            return successor;
+        } 
+        root = root.right;
+        while (root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+}
---
+++ Summer/Binary Tree/106. Convert Sorted List to Binary Search Tree .java
@@ -0,0 +1,78 @@
+/*106. Convert Sorted List to Binary Search Tree
+中文English
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+Example
+Example 1:
+	Input: array = [1,2,3]
+	Output:
+		 2  
+		/ \
+		1  3
+		
+Example 2:
+	Input: [2,3,6,7]
+	Output:
+		 3
+		/ \
+	       2   6
+		     \
+		      7
+
+	Explanation:
+	There may be multi answers, and you could return any of them.
+null
+*/
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param head: The first node of linked list.
+     * @return: a tree node
+     */
+    public TreeNode sortedListToBST(ListNode head) {
+        // write your code here
+        List<Integer> list = new ArrayList<>();
+        ListNode curt = head;
+        while (curt != null){
+            list.add(curt.val);
+            curt = curt.next;
+        }
+        return buildTree(list, 0, list.size()-1);
+    }
+    private TreeNode buildTree(List<Integer> list,int start, int end){
+        if (start>end){
+            return null;
+        } 
+        if (start == end){
+            return new TreeNode(list.get(start));
+        } 
+        int mid = (start + end)/2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = buildTree(list,start,mid-1);
+        root.right = buildTree(list,mid+1,end);
+        return root;
+        
+    }
+   
+}
